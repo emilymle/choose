9840272c73c1452c096aaadcbabda4faf2a73fbf
Emily <emilylewismft@gmail.com>
2015-06-24 21:01:05 -0700

    initial commit

---
+++ choose.js
@@ -0,0 +1,7 @@
+jimmyCar = {make: "mercedes", model: "sprinter", color: "gray", year: 2005};
+  var Car = function(){
+  this.make= "chevy";
+  this.modle= "malibu"
+}
+tavoCar= new Car("ferrari");
+jessieCar= new Car("scion");
