103bea08d2878cdeae979c819688e81acb220bc0
Emily <emilylewismft@gmail.com>
2015-06-24 22:41:17 -0700

    builds javascript logic

--- choose.js
+++ choose.js
@@ -1,7 +1,14 @@
-jimmyCar = {make: "mercedes", model: "sprinter", color: "gray", year: 2005};
-  var Car = function(){
-  this.make= "chevy";
-  this.modle= "malibu"
-}
-tavoCar= new Car("ferrari");
-jessieCar= new Car("scion");
+var wdi_17_names = ["Addison", "Ben", "Christine", "Chloe", "Cris", "Dane", "David H", "David Z", "Emily", "Imtaek", "Jack", "Jesse P", "Jesse Z", "Jonathan", "Pericles", "Peter", "Philippe", "Sergio", "Simon", "Tali", "Tavo", "Trevor"];
+
+var nameLoop = function() {
+  for (i = 0; i < wdi_17_names.length; i++) {
+  console.log(wdi_17_names[i]);
+}};
+
+// nameLoop();
+
+var randomName = function() {
+  var arrayPosition = Math.floor((Math.random() * 22) + 1);
+  console.log(wdi_17_names[arrayPosition]);
+};
+randomName();
